a4ffe995eee883321f124eacba42b85c360e988c
Shreya Shrestha <shreyastha84@gmail.com>
2024-02-25 21:43:16 +0545

    update model

--- scooter_application/Scooter/models.py
+++ scooter_application/Scooter/models.py
@@ -1,18 +1,21 @@
 from django.db import models
+from User.models import User
 
 # Create your models here.
 class Scooter(models.Model):
-    name = models.CharField(max_length = 100)
-    username = models.CharField(max_length = 100,primary_key = True)
-    password = models.CharField(max_length = 100)
-    confirm_password = models.CharField(max_length = 100,null = True)
-    address = models.CharField(max_length = 500,null = True)
-    age = models.IntegerField( default=18,
-        validators=[
-            MinValueValidator(18)
-        ])
-    amount = models.FloatField(default=0)
+    id = models.IntegerField(primary_key = True)
+    color = models.CharField(max_length = 50)
+    model = models.CharField(max_length = 100)
+    rate_per_hour = models.FloatField(default = 0.0)
+    user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_active = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.name
+        return self.model
+    
+    @property
+    def time_diff(self,start_time,end_time):
+        return self.end_time - self.start_time
 
